d49f43c7e586dc6786f8d4c64a591d7a3f788165
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-02 21:39:10 -0500

    clean up

--- server.js
+++ server.js
@@ -37,53 +37,6 @@ var waitList = [];
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// app.get("/", function(req, res) {
-//   res.send("Welcome to the Page!");
-// });
-
-// app.get('/', function(req, res){
-//     res.sendFile(path.join(__dirname + '/app/public/home.html'));
-// });
-
-// app.get('/reservations', function(req, res){
-//     res.sendFile(path.join(__dirname + '/app/public/reservations.html'));
-// });
-
-// app.get('/tables', function(req, res){
-//     res.sendFile(path.join(__dirname + '/app/public/tables.html'));
-// })
-
-// app.get('/api/:tableData?', function(req, res){
-
-//    res.sendFile(path.join(__dirname + '/app/data/tableData.js'));
-    
-//     var customer = req.params.tableData;
-
-//    if(customer){
-//         console.log(customer);
-
-//        for (var i=0; i <tableData.length; i++){
-
-//            if (customer == tableData[i].routeName){
-//                 res.json(tableData[i]);
-//                 return;
-//             }
-//         }
-
-//        res.json(false);
-//     }
-
-//    else{
-//         res.json(tableData);
-//     }
-// })
-
-// app.post('/api/new', function(req, res){
-    
-// })
-
-// };
-
 // ==============================================================================
 // LISTENER
 // The below code effectively "starts" our server
